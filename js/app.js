65d0d3858f0c872f1a954d9003c64d4e2b619611
Nnanna <nnanna_okoli@hotmail.com>
2022-04-01 12:18:31 +0100

    2nd Draft

--- js/app.js
+++ js/app.js
@@ -27,3 +27,30 @@ toggleLines = () => {
 
 
 }
+
+
+
+//// accordions
+
+// This code was an attempt but does not seem to do the job - can/will delete
+
+var sections = document.querySelectorAll('section');
+
+// https://developer.mozilla.org/en-US/docs/Web/Events/hashchange
+window.addEventListener('hashchange', function(event) {
+  removeAllActiveClasses();
+  activateSectionFromHash();
+});
+
+function removeAllActiveClasses() {
+  [].forEach.call(sections, function(element) {
+    element.classList.remove('active');
+  });
+}
+
+function activateSectionFromHash() {
+  // hash starts with #, getElementById searches without
+  document.getElementById(location.hash.substring(1)).classList.add('active');
+}
+
+//
